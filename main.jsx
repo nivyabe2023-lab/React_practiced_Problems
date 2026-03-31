adb57e338f581032f8a554daf1dee74926baddf3
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-31 23:50:10 +0530

    main

--- main.jsx
+++ main.jsx
@@ -1,35 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import Home from "./Home";
-import Login from "./Login";
-import Counter from "./Counter";
-import NotFound from "./NotFound";
-import Post from "./post";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <NotFound />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/counter",
-    element: <Counter value={10} />
-  },
-  {
-    path:"/Post/:id",
-    element:<Post/>
-  }
-]);
+import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
